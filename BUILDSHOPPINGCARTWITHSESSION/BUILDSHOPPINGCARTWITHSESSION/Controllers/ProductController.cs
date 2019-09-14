6ae01be7072766233df8f8ff2f12f8688ad74ad4
sanshanthan <sanshanthan@yahoo.com>
2019-09-14 19:17:28 +0530

    SIMPLE CART IMPLEMENTED

---
+++ BUILDSHOPPINGCARTWITHSESSION/BUILDSHOPPINGCARTWITHSESSION/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using BUILDSHOPPINGCARTWITHSESSION.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BUILDSHOPPINGCARTWITHSESSION.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ProductModel productModel = new ProductModel();
+            ViewBag.products = productModel.findAll();
+            return View();
+
+        }
+    }
+}
